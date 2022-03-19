9c89f752c778c8fdfe566a350575e9bd5860519b
Simon Schmitke <simo.schm@gmail.com>
2022-03-19 13:57:22 -0400

    First commit

---
+++ app/models/game.rb
@@ -0,0 +1,24 @@
+class Game
+    def self.find_winner_for_game(game:)
+        game.teams["away"]["score"] > game.teams["home"]["score"] ? "away" : "home"
+    end
+
+    def self.find_winner_for_games(games:)
+        outcome = {
+            home: 0,
+            away: 0
+        }
+        games.each do | game | 
+            winner = find_winner_for_game(game: game)
+            winner == "home" ? outcome[:home]+=1 : outcome[:away]+=1
+        end
+
+        outcome
+        
+    end   
+
+    def home_team_win_pct_1yr
+        # Calculate the number of wins by the home team, divided by number of wins by away team.
+
+    end
+end
